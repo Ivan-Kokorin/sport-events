e9870879c17f465cbde223341bc07b02565940dc
Ivan-Kokorin <kokorin@cons411.ru>
2021-09-28 04:29:35 +0800

    add rabbit config for sport

---
+++ sport/src/main/java/com/application/sport/RabbitConfiguration.java
@@ -0,0 +1,36 @@
+package com.application.sport;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfiguration {
+    public static final String QUEUE_MESSAGES = "parse-messages-queue";
+    public static final String HOSTNAME = "localhost";
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory(HOSTNAME);
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
+    }
+
+    @Bean
+    public Queue myQueue() {
+        return new Queue(QUEUE_MESSAGES);
+    }
+}
--- sport/src/main/java/com/application/sport/SportApplication.java
+++ sport/src/main/java/com/application/sport/SportApplication.java
@@ -1,9 +1,14 @@
 package com.application.sport;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Import(RabbitConfiguration.class)
+@EnableRabbit
 public class SportApplication {
 
 	public static void main(String[] args) {
