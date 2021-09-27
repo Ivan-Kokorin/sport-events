91ac64ebb2a61ebd6e300c625f0171a27fa3febf
Ivan-Kokorin <kokorin@cons411.ru>
2021-09-28 02:43:19 +0800

    add configuration rabbitmq

--- parser/src/main/java/com/data/parser/ParserApplication.java
+++ parser/src/main/java/com/data/parser/ParserApplication.java
@@ -1,13 +1,19 @@
 package com.data.parser;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@Import(RabbitConfiguration.class)
+@EnableRabbit
+@EnableScheduling
 public class ParserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ParserApplication.class, args);
+    }
 
 }
